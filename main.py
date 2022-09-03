8b09f720677b9a5377d40f7509ddee7106efafe1
Brandon Hong <hongbra@oregonstate.edu>
2022-09-03 12:57:52 -0700

    Start of algorithm learning

---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+def find_minimum_depth(root):
+    # TODO: Write your code here
+    queue = []
+    depth = 1
+    queue.append(root)
+    while queue:
+        size = len(queue)
+        for _ in range(size):
+            node = queue.pop(0)
+
+            if node.left is None and node.right is None:
+                return depth
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        depth += 1
+    return depth
+
+
+if __name__ == '__main__':
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+
